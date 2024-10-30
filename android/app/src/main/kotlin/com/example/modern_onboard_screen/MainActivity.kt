a0315401af195efc31ddf2f04bbe0b62f49584df
Mattha1401 <hataimas.supradit@gmail.com>
2024-10-31 00:06:35 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/modern_onboard_screen/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.modern_onboard_screen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screen/introduction_screen.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'home_screen.dart';
+
+
+class IntroScreen extends StatelessWidget {
+  IntroScreen({super.key});
+
+  final List<PageViewModel> pages = [
+    PageViewModel(
+      title: 'Connect With Alien',
+      body: 'Sitting all alone Waiting for you to come home Wanna know about your everythin When the day is over',
+      // title: 'First Page',
+      // body: 'Description',
+      footer: SizedBox(
+        height: 45,
+        width: 150,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            // primary: Colors.green,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            elevation: 8,
+          ),
+          onPressed: () {},
+          child: const Text("Let's Go"),
+        ),
+      ),
+      image: Center(
+        child: Image.asset('images/c1.jpg')
+      ),
+      decoration: const PageDecoration(
+        titleTextStyle: TextStyle(
+          fontSize: 25.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    ),
+    PageViewModel(
+      title: 'Have Access TRANSFORMER!',
+      body: 'Broken heart, just put it all on me Maybe you just cannot hear a thing Say a little prayer that you can feel my love For you',
+      footer: SizedBox(
+        height: 45,
+        width: 150,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            // primary: Colors.green,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            elevation: 8,
+          ),
+          onPressed: () {},
+          child: const Text("Why to wait!"),
+        ),
+      ),
+      image: Center(
+        child: Image.asset('images/c2.jpg'),
+      ),
+      decoration: const PageDecoration(
+        titleTextStyle: TextStyle(
+          fontSize: 25.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    ),
+    PageViewModel(
+      title: 'Here We Start g(h)o(st)!',
+      body: 'I will always be your home You will never walk alone Even if the wind is gonna blow Even if the rain is gonna pour, love',
+      footer: SizedBox(
+        height: 45,
+        width: 150,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            elevation: 8,
+          ),
+          onPressed: () {},
+          child: const Text("Let's Start"),
+        ),
+      ),
+      image: Center(
+        child: Image.asset('images/c3.jpg'),
+      ),
+      decoration: const PageDecoration(
+        titleTextStyle: TextStyle(
+          fontSize: 25.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Because of you i shine'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(12, 80, 12, 12),
+        child: IntroductionScreen(
+          pages: pages,
+          dotsDecorator: const DotsDecorator(
+            size: Size(15, 15),
+            color: Color.fromARGB(255, 241, 83, 204),
+            activeSize: Size.square(20),
+            activeColor: Color.fromARGB(255, 255, 241, 93),
+          ),
+          showDoneButton: true,
+          done: const Text(
+            'DONE go to galaxy',
+            style: TextStyle(fontSize: 20),
+          ),
+          showSkipButton: true,
+          skip: const Text(
+            'Skip to Mars',
+            style: TextStyle(fontSize: 20),
+          ),
+          showNextButton: true,
+          next: const Icon(
+            Icons.arrow_forward,
+            size: 25,
+          ),
+          onDone: () => onDone(context),
+          curve: Curves.bounceOut,
+        ),
+      ),
+    );
+  }
+
+  void onDone(context) async {
+   final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('ON_BOARDING', false);
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const HomeScreen()));
+  }
+}
+
+// เก็บข้อมูล Key-value ลงในเครื่องแบบถาวรด้วย Shared Preferences
